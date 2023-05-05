74fbd150d27552a4608d4c7d1f8bd8420319332a
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-05-04 23:00:34 -0300

    Add project files.

---
+++ NumerosAleatoriosIO/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NumerosAleatoriosIO.Services;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("/n Usamos el nuevo generador /n");
+GeneradorService GeneradorService = new();
+
+var num2 = GeneradorService.ParteCentralDelCuadrado(3,123,4);
+foreach (var item in num2)
+{
+    Console.WriteLine(item);
+}
---
+++ NumerosAleatoriosIO/Services/GeneradorService.cs
@@ -0,0 +1,37 @@
+﻿namespace NumerosAleatoriosIO.Services
+{
+    public class GeneradorService
+    {
+        public List<double> ParteCentralDelCuadrado(int digitosDeseados, int semilla, int totalDeNumerosaGenerar)
+        {
+            
+            List<double> u = new List<double>();
+            for (int i = 1;i <= totalDeNumerosaGenerar;i++)
+            {
+                int x = semilla * semilla;
+                semilla = ((x.ToString().Length - digitosDeseados) % 2 == 0)
+                            ? NumerosCentrales(digitosDeseados, x)
+                            : NumerosCentrales(digitosDeseados, x * 10);
+                u.Add(double.Parse("0," + semilla));
+            }
+            return u;
+        }
+
+        private int NumerosCentrales(int numeroDeDigitosDeseados, int cuadradoDeLaSemilla)
+        {
+            string xstr = cuadradoDeLaSemilla.ToString();
+            int digitosDescartados = (xstr.Length - numeroDeDigitosDeseados) / 2;
+            xstr = xstr.Remove(0, digitosDescartados);
+            xstr = xstr.Remove(xstr.Length - digitosDescartados, digitosDescartados);
+            int numerosCentrales = Int32.Parse(xstr);
+            return numerosCentrales;
+        }
+
+        public List<double> MetodoDeLehmer(int semilla, int kDigitos)
+        {
+            var some = semilla * kDigitos;
+
+            return null;
+        }
+    }
+}
